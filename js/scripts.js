6875cec7cdd0b700673dd7aba132f0d0e9939f02
Student <example@example.com>
2019-10-23 12:03:01 -0700

    adds new formulas and user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-var gallons = parseInt(prompt("Gallons:"))
-
 
 function toLiters (gallons) {
 return gallons / 0.26417;
-}
+};
 
-alert(toLiters(gallons))
+function toGrams (ounces) {
+  return ounces * 28.3495;
+};
+
+
+
+$(document).ready(function() {
+  $('form#galLiters').submit(function(event) {
+    event.preventDefault();
+    var gallons = parseInt($('#galLiters-input').val());
+    var result = toLiters(gallons).toFixed(2);
+    $('#galLiters-output').text(result + " liters");
+  });
+  $('form#toOunces').submit(function(event) {
+    event.preventDefault();
+    var ounces = parseInt($('#ounceGrams-input').val());
+    var result = toGrams(ounces).toFixed(2);
+    $('#ounceGrams-output').text(result + " Grams");
+  });
+});
